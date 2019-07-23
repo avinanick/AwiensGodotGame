d9cc39a7c59793aefea97c2b93a581218d19c729
avinanick <avinanick@gmail.com>
2019-07-22 20:41:35 -0700

    Modified the position of the player avatar to match the position of the turrets they will be using.

--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ extends Spatial
 var rotation_speed = 1
 onready var selected_turret = get_node("/root/MainScene/EastGun")
 onready var main_scene = get_node("/root/MainScene")
+onready var camera = get_node("Camera")
 
 # Called when the node enters the scene tree for the first time.
 # This all seems to be working right now, I'd like to change up the way it looks to interact with turret objects better
@@ -22,29 +23,30 @@ func _process(delta):
 		if Input.is_action_just_pressed("select_one"):
 			# select the north gun (0,3.5,13) (0,0,0)
 			selected_turret = get_node("/root/MainScene/NorthGun")
-			translation = Vector3(0,3.5,13)
+			translation = Vector3(0,2.2,13)
 			rotation_degrees = Vector3(0,180,0)
 			print("switch to turret one")
 		if Input.is_action_just_pressed("select_two"):
 			# select the south gun (0,3.5,-13)(0,180,0)
 			selected_turret = get_node("/root/MainScene/SouthGun")
-			translation = Vector3(0,3.5,-13)
+			translation = Vector3(0,2.2,-13)
 			rotation_degrees = Vector3(0,0,0)
 			print("switch to turret two")
 		if Input.is_action_just_pressed("select_three"):
 			# select the east gun (13,3.5,0)(0,-90,0)
 			selected_turret = get_node("/root/MainScene/EastGun")
-			translation = Vector3(13,3.5,0)
+			translation = Vector3(13,2.2,0)
 			rotation_degrees = Vector3(0,-90,0)
 			print("switch to turret three")
 		if Input.is_action_just_pressed("select_four"):
 			# select the west gun (-13,3.5,0)(0,90,0)
 			selected_turret = get_node("/root/MainScene/WestGun")
-			translation = Vector3(-13,3.5,0)
+			translation = Vector3(-13,2.2,0)
 			rotation_degrees = Vector3(0,90,0)
 			print("switch to turret four")
 		if Input.is_action_pressed("fire_one"):
-			selected_turret.fire(translation, rotation) # crashes if the targetted turret was destroyed
+			# I need to modify this to accurately spawn at the camera position instead of base player
+			selected_turret.fire(camera.get_global_transform().origin, rotation) # crashes if the targetted turret was destroyed
 	
 # This I think should be called whenever an input event happens
 func _input(event):
