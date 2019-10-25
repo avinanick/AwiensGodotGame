4e2d688530b98366d373d9138eb7863da688dbfc
avinanick <avinanick@gmail.com>
2019-10-25 09:26:21 -0700

    Fixed a typo with detecting input on the turret replacement section. Next I need to get the actual replacements working.

--- Scripts/Weapons/Turret.gd
+++ Scripts/Weapons/Turret.gd
@@ -41,6 +41,6 @@ func fire(start_location : Vector3, start_rotation : Vector3):
 		print("attempting to fire dead turret")
 
 func _on_ProtoGun_input_event(camera, event, click_position, click_normal, shape_idx):
-	if event.is_action_just_presed("fire_one") and main_scene.game_state == main_scene.game_states.transitioning:
+	if event.is_action_pressed("fire_one") and main_scene.game_state == main_scene.game_states.transitioning:
 		main_scene.selected_turret = self
 	
