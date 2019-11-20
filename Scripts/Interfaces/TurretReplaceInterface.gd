6202979e43fef2cbfd67d85aabb8c67c1baf80fc
avinanick <avinanick@gmail.com>
2019-11-19 21:43:16 -0800

    Updated the replace interface to work with the thunder cannon scene, I should likely change the way this works form a match to a dictionary at some point.

--- Scripts/Interfaces/TurretReplaceInterface.gd
+++ Scripts/Interfaces/TurretReplaceInterface.gd
@@ -4,6 +4,7 @@ onready var main_scene := get_node("/root/MainScene") as GameManager
 
 var flak_cannon_scene = preload("res://Scenes/Units/FlakCannon.tscn")
 var chaingun_scene = preload("res://Scenes/Units/ProtoGun.tscn")
+var thunder_cannon_scene = preload("res://Scenes/Units/ThunderCannon.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,6 +25,8 @@ func turret_selected(var turret_type: String, var turret_location: String):
 			main_scene.replace_turret(chaingun_scene, turret_location)
 		"Flak Cannon":
 			main_scene.replace_turret(flak_cannon_scene, turret_location)
+		"Thunder Cannon":
+			main_scene.replace_turret(thunder_cannon_scene, turret_location)
 			
 func update_turret_options():
 	get_node("Panel/TitleElementSeparator/TurretsVSeparation/TurretsHSeparationTop/NorthTurretList/TurretSelectionDropdown").update_turret_list()
