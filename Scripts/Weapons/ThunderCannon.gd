40c3d49877cdd7ffceb298a18b29574e6f63f2ac
avinanick <avinanick@gmail.com>
2019-11-19 21:06:53 -0800

    The scene for the thunder cannon should be finished, but the range needs to be balanced I'm sure, and a lightning effect should be created for the cannon firing.

--- Scripts/Weapons/ThunderCannon.gd
+++ Scripts/Weapons/ThunderCannon.gd
@@ -1,8 +1,8 @@
 extends Turret
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var targeting_zone = get_node("TargetingZone")
+
+export var lightning_damage: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,5 +16,29 @@ func fire(start_location : Vector3, start_rotation : Vector3):
 	# This should rotate the targetingZone to where the camera is looking and check if there are any enemies in that
 	# zone, if the cooldown is ready. TargetingZone can be rotated directly, and will work perfectly
 	if is_alive and timer >= fire_cooldown:
-		timer = 0
-	pass
+		if targeting_zone:
+			targeting_zone.rotation = start_rotation
+			var target = scan_for_targets()
+			if target:
+				timer = 0
+				target.take_damage(lightning_damage)
+	
+func scan_for_targets():
+	# Checks if there are any valid targets in the targeting zone area, if so, returns the closest one
+	var possible_targets = targeting_zone.get_overlapping_areas()
+	var target = null
+	if possible_targets.size() > 0:
+		var smallest_distance: int
+		for possible_target in possible_targets:
+			var distance = self.get_global_transform().origin.distance_to(possible_target.get_global_transform().origin)
+			if target:
+				if possible_target.is_in_group("Aliens") and distance < smallest_distance:
+					target = possible_target
+					smallest_distance = distance
+			else:
+				if possible_target.is_in_group("Aliens"):
+					target = possible_target
+					smallest_distance = distance
+	else:
+		print("No targets in range")
+	return target
