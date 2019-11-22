afdc790c8b9c08772b25c0bb9291ee9517b018db
avinanick <avinanick@gmail.com>
2019-11-21 22:14:37 -0800

    Created the scene and script for the energy shield. It still needs proper connections, animations, and an upgrade to go with it though.

---
+++ Scripts/EnergyShield.gd
@@ -0,0 +1,31 @@
+extends Structure
+
+export var recovery_per_second: float = 1
+var recovery_timer: float = 0
+var time_per_recover: float
+var overloaded: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	time_per_recover = 1 / recovery_per_second
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	recovery_timer += delta
+	if overloaded:
+		if recovery_timer >= (self.max_health * time_per_recover):
+			recovery_timer = 0
+			overloaded = false
+			self.health = self.max_health
+			get_node("CollisionShape").disabled = false
+	elif recovery_timer >= time_per_recover:
+		recovery_timer = 0
+		if health < max_health:
+			health += 1
+			emit_signal("health_changed", self)
+			
+func take_damage(var amount: int):
+	.take_damage(amount)
+	if self.health <= 0:
+		overloaded = true
+		get_node("CollisionShape").disabled = true
