bc982d9f316aa81419a7d868280b8f407c53b764
avinanick <avinanick@gmail.com>
2019-12-24 22:46:03 -0800

    The turret replace interface should be done now, the upgrades interface seems mostly finished, but needs it's connections set up.

--- Scripts/Interfaces/TurretReplaceInterface.gd
+++ Scripts/Interfaces/TurretReplaceInterface.gd
@@ -1,34 +1,41 @@
 extends MarginContainer
 
-onready var main_scene := get_node("/root/MainScene") as GameManager
+var turret_type_dict := {"Chaingun" : preload("res://Scenes/Units/ProtoGun.tscn"),
+	"Flak Cannon" : preload("res://Scenes/Units/FlakCannon.tscn"),
+	"Thunder Cannon" : preload("res://Scenes/Units/ThunderCannon.tscn")}
 
-var flak_cannon_scene = preload("res://Scenes/Units/FlakCannon.tscn")
-var chaingun_scene = preload("res://Scenes/Units/ProtoGun.tscn")
-var thunder_cannon_scene = preload("res://Scenes/Units/ThunderCannon.tscn")
-
-signal turret_type_selected
+signal north_turret_type_selected
+signal south_turret_type_selected
+signal east_turret_type_selected
+signal west_turret_type_selected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.connect("north_turret_type_selected", get_node("../Guns/NorthGun"), "replace_turret")
+	self.connect("south_turret_type_selected", get_node("../Guns/SouthGun"), "replace_turret")
+	self.connect("east_turret_type_selected", get_node("../Guns/EastGun"), "replace_turret")
+	self.connect("west_turret_type_selected", get_node("../Guns/WestGun"), "replace_turret")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_ContinueButton_button_up():
-	main_scene.start_level_preparation()
+	get_node("../UpgradesInterface").visible = true
+	self.visible = false
 	
 
 func turret_selected(var turret_type: String, var turret_location: String):
 	print("Selected turret type is ", turret_type)
-	match turret_type:
-		"Chaingun":
-			main_scene.replace_turret(chaingun_scene, turret_location)
-		"Flak Cannon":
-			main_scene.replace_turret(flak_cannon_scene, turret_location)
-		"Thunder Cannon":
-			main_scene.replace_turret(thunder_cannon_scene, turret_location)
+	match turret_location:
+		"North":
+			emit_signal("north_turret_type_selected", self.turret_type_dict[turret_type])
+		"South":
+			emit_signal("south_turret_type_selected", self.turret_type_dict[turret_type])
+		"East":
+			emit_signal("east_turret_type_selected", self.turret_type_dict[turret_type])
+		"West":
+			emit_signal("west_turret_type_selected", self.turret_type_dict[turret_type])
 			
 func update_turret_options():
 	get_node("Panel/TitleElementSeparator/TurretsVSeparation/TurretsHSeparationTop/NorthTurretList/TurretSelectionDropdown").update_turret_list()
