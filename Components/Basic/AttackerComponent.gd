3100e37781a3c3fb05a878e58fe650ec6eaa5800
avinanick <avinanick@gmail.com>
2020-12-11 10:14:07 -0800

    Started the basic response to firing on the attacker component. The fire function itself doesn't do anything yet.

--- Components/Basic/AttackerComponent.gd
+++ Components/Basic/AttackerComponent.gd
@@ -1,16 +1,25 @@
 extends "res://Components/Basic/Component.gd"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var projectile: PackedScene = preload("res://Scenes/Bullet.tscn")
+export var projectile_damage: int = 2
+
+signal primary_attack
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.connect("primary_attack", self, "fire")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func fire():
+	pass
+
+func process_message(var message: String, var args: Dictionary):
+	if message == "fire_primary":
+		emit_signal("primary_attack")
