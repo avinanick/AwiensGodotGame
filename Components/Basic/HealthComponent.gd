9a606da8cb60a6546a48f08129a775ae7563eeb6
avinanick <avinanick@gmail.com>
2020-12-11 10:02:00 -0800

    Added a repair damage response in the health component.

--- Components/Basic/HealthComponent.gd
+++ Components/Basic/HealthComponent.gd
@@ -2,25 +2,34 @@ extends "res://Components/Basic/Component.gd"
 
 
 export var health_points: int = 10
+export var max_health: int = 10
 
 signal incoming_damage
+signal incoming_repair
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("incoming_damage", self, "take_damage")
-	pass # Replace with function body.
+	self.connect("incoming_repair", self, "repair_damage")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func process_message(var message: String, var args: Dictionary):
+	if message == "deal_damage":
+		emit_signal("incoming_damage", args["damage"])
+	if message == "repair_damage":
+		emit_signal("incoming_repair", args["repair"])
+
+func repair_damage(var amount: int):
+	self.health_points += amount
+	if self.health_points > self.max_health:
+		self.health_points = max_health
+
 func take_damage(var amount: int):
 	self.health_points -= amount
 	if self.health_points <= 0:
 		get_parent().propagate_message("self_destroyed", {})
-
-func process_message(var message: String, var args: Dictionary):
-	if message == "deal damage":
-		emit_signal("incoming_damage", args["damage"])
