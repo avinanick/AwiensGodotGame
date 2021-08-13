b9730ffa9d8b70122b55f61799aaf9db5f30b268
avinanick <avinanick@gmail.com>
2021-08-12 22:15:07 -0700

    Starting some refactoring using c# in the component system.

--- Components/Basic/ControlsComponent.cs
+++ Components/Basic/ControlsComponent.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Component : Node
+public class ControlsComponent : Component
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
--- Components/Basic/MobilityComponent.gd
+++ Components/Basic/MobilityComponent.gd
@@ -14,3 +14,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func set_direction(var direction: Vector3):
+	pass
+	
+func set_orbit(var center: Vector3):
+	pass
+	
+func set_target(var target: Node):
+	pass
