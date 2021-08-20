6e1d628c8213b59b457f8892a0c787fb0fb4201c
avinanick <avinanick@gmail.com>
2021-08-20 11:40:14 -0700

    Added the basic functionality to the turret model class code.

--- Resources/Models/Units/Earthlings/Turrets/TurretModel.cs
+++ Resources/Models/Units/Earthlings/Turrets/TurretModel.cs
@@ -6,11 +6,14 @@ public class TurretModel : Spatial
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	private Spatial BaseBone;
+	private Spatial GunBone;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GunBone = GetNode<Spatial>("Bone0/Bone1");
+		BaseBone = GetNode<Spatial>("Bone0");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +21,19 @@ public class TurretModel : Spatial
 //  {
 //      
 //  }
+
+public void Sight(float XRotation, float YRotation) {
+		Vector3 horizontalRotation = BaseBone.RotationDegrees;
+		horizontalRotation.x -= XRotation;
+		Vector3 verticalRotation = GunBone.RotationDegrees;
+		verticalRotation.y -= YRotation;
+		
+		BaseBone.RotationDegrees = horizontalRotation;
+		GunBone.RotationDegrees = verticalRotation;
+	}
+	
+	public void ResetSights() {		
+		GunBone.RotationDegrees = new Vector3(0,0,0);
+		BaseBone.RotationDegrees = new Vector3(0,0,0);
+	}
 }
