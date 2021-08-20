34e57fe202ead46569789b28621e05c66c6f95d9
avinanick <avinanick@gmail.com>
2021-08-20 13:41:28 -0700

    Added variables to the turret code for the attacker component and turret model.

--- GameObjects/Units/Earthlings/Turret.cs
+++ GameObjects/Units/Earthlings/Turret.cs
@@ -6,12 +6,13 @@ public class Turret : Destructible
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
-	private AttackerComponent Blah;
+	protected AttackerComponent Attacker;
+	protected TurretModel Model;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		AttackerComponent attacker = GetNode<AttackerComponent>("Attacker");
+		Attacker = GetNode<AttackerComponent>("Attacker");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
