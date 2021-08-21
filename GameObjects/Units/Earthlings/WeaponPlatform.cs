22a0fc40ffc8ee592475fcc9cc31f300cc53e5af
avinanick <avinanick@gmail.com>
2021-08-21 10:54:24 -0700

    Added some basic functionality to the weapon platform class to pass down control calls.

--- GameObjects/Units/Earthlings/WeaponPlatform.cs
+++ GameObjects/Units/Earthlings/WeaponPlatform.cs
@@ -6,11 +6,12 @@ public class WeaponPlatform : Spatial
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	private Turret CurrentWeapon;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		CurrentWeapon = GetNode<Turret>("AntiAirTurret");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +19,20 @@ public class WeaponPlatform : Spatial
 //  {
 //      
 //  }
+
+	public void AttemptAttack(Vector3 attackSpawn, Vector3 attackRotation) {
+		CurrentWeapon.AttemptAttack(attackSpawn, attackRotation);
+	}
+	
+	public void ResetSights() {
+		CurrentWeapon.ResetSights();
+	}
+	
+	public void Sight(float xRotation, float yRotation) {
+		CurrentWeapon.Sight(xRotation, yRotation);
+	}
+
+	public void SwitchWeapon(string weaponPath) {
+		
+	}
 }
