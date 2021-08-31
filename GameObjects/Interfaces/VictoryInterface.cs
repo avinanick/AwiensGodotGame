78b8ccf156c74709c1088f9ea92adeacbc8c391b
avinanick <avinanick@gmail.com>
2021-08-30 22:24:15 -0700

    Added stub methods to the victory interface class.

--- GameObjects/Interfaces/VictoryInterface.cs
+++ GameObjects/Interfaces/VictoryInterface.cs
@@ -10,7 +10,7 @@ public class VictoryInterface : PanelContainer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		base._Ready();
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +18,40 @@ public class VictoryInterface : PanelContainer
 //  {
 //      
 //  }
+
+	public void AddKillIcon() {
+		
+	}
+	
+	public void ClearKillIcons() {
+		
+	}
+
+	public void ContinueGame() {
+		
+	}
+	
+	public void EnemyDefeated() {
+		
+	}
+
+	public void PlayerDefeat() {
+		
+	}
+	
+	public void PlayerVictory() {
+		
+	}
+	
+	public void QuitGame() {
+		
+	}
+	
+	public void SaveAndQuit() {
+		
+	}
+	
+	public void UpdateScore() {
+		
+	}
 }
