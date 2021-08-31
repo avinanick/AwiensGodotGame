49b8af6e3976262efd5ade41da5855708d83763b
avinanick <avinanick@gmail.com>
2021-08-30 18:15:41 -0700

    Set the default ready behavior of the pause menu.

--- GameObjects/Interfaces/PauseMenu.cs
+++ GameObjects/Interfaces/PauseMenu.cs
@@ -10,7 +10,8 @@ public class PauseMenu : PanelContainer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		base._Ready();
+		Visible = false;
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
