65927bd07cbdf7068fe3d20095e620a7c31d7cd1
avinanick <avinanick@gmail.com>
2021-09-03 15:16:43 -0700

    Set up the handle collision method for the alien missile class.

--- GameObjects/Units/Aliens/AlienMissile.cs
+++ GameObjects/Units/Aliens/AlienMissile.cs
@@ -12,12 +12,21 @@ public class AlienMissile : Ship
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		base._Ready();
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+	}
+
+	public override void HandleCollision(KinematicCollision collision) {
+		if(collision != null) {
+			if(collision.Collider is Destructible destructible) {
+				destructible.TakeDamage(MissileDamage);
+			}
+			DestroySelf();
+		}
+	}
 }
