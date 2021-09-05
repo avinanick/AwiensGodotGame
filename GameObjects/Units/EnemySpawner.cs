6d89c71e629b422cfaf7d4bb2a540783ce084728
avinanick <avinanick@gmail.com>
2021-09-04 18:21:22 -0700

    Wrote out the initial function for spawn enemy in the enemy spawner class.

--- GameObjects/Units/EnemySpawner.cs
+++ GameObjects/Units/EnemySpawner.cs
@@ -16,11 +16,13 @@ public class EnemySpawner : Spatial
     private float SpawnPeriod = 2.0f;
     private bool Spawning = true;
     private PackedScene EnemyScene;
+    private Random RNG;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         base._Ready();
+        RNG = new Random();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -51,7 +53,13 @@ public class EnemySpawner : Spatial
     }
 
     public void SpawnEnemy() {
-
+        float spawnAngleRadians = (float)RNG.NextDouble() * 2 * Mathf.Pi;
+        float xValueSpawn = Mathf.Cos(spawnAngleRadians) * SpawnRadius;
+        float zValueSpawn = Mathf.Sin(spawnAngleRadians) * SpawnRadius;
+        Ship newEnemy = (Ship)EnemyScene.Instance();
+        GetTree().CurrentScene.AddChild(newEnemy);
+        newEnemy.Translation  = new Vector3(xValueSpawn, GlobalTransform.origin.y, zValueSpawn);
+        newEnemy.SpawnShip();
     }
 
     private void UpdateDifficulty() {
