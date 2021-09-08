03794192be07a57c4b52261824fdcfe7ec33d8f0
avinanick <avinanick@gmail.com>
2021-09-07 18:10:25 -0700

    Set the campaing tracker to call its base ready method, probably not needed but good practice for godot.

--- GameSystems/CampaignTracker.cs
+++ GameSystems/CampaignTracker.cs
@@ -22,7 +22,7 @@ public class CampaignTracker : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
