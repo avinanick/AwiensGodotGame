70b2ec0aca42e852bc63be4ee1774f727732cab6
avinanick <avinanick@gmail.com>
2021-09-08 21:19:00 -0700

    Added member variables to the radar indicator class.

--- GameObjects/Interfaces/RadarIndicator.cs
+++ GameObjects/Interfaces/RadarIndicator.cs
@@ -6,6 +6,10 @@ public class RadarIndicator : Sprite
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private Texture AllyIndicatorTexture = (Texture)GD.Load("res://Resources/Interfaces/dotGreen.png");
+    private Color AllyModulate = new Color(1, 1, 1);
+    private Texture EnemyIndicatorTexture = (Texture)GD.Load("res://Resources/Interfaces/dotRed.png");
+    private Color EnemyModulate = new Color(1, 0, 0);
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
