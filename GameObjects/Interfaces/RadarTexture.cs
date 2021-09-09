56e665d9c0156996dba7da59dec75bf193d5a2a5
avinanick <avinanick@gmail.com>
2021-09-08 21:45:33 -0700

    Updated methods in the radar texture class to make use of the radar indicators.

--- GameObjects/Interfaces/RadarTexture.cs
+++ GameObjects/Interfaces/RadarTexture.cs
@@ -7,13 +7,9 @@ public class RadarTexture : TextureRect
 	// private int a = 2;
 	// private string b = "text";
 	[Export]
-	private PackedScene EnemyIndicator = (PackedScene)GD.Load("res://GameObjects/Interfaces/EnemyRadarIndicator.tscn");
-	[Export]
-	private PackedScene AllyIndicator = (PackedScene)GD.Load("res://GameObjects/Interfaces/AllyRadarIndicator.tscn");
+	private PackedScene Indicator = (PackedScene)GD.Load("res://GameObjects/Interfaces/RadarIndicator.tscn");
 
 	private Light2D PlayerView;
-	private System.Collections.Stack SpareAllyIndicators = new System.Collections.Stack();
-	private System.Collections.Stack SpareEnemyIndicators = new System.Collections.Stack();
 
 
 	// Called when the node enters the scene tree for the first time.
@@ -29,31 +25,14 @@ public class RadarTexture : TextureRect
 //      
 //  }
 
-	private void ClearIndicators() {
-		Godot.Collections.Array<Node> enemyIndicators = new Godot.Collections.Array<Node>(GetTree().GetNodesInGroup("EnemyIndicators"));
-		Godot.Collections.Array<Node> allyIndicators = new Godot.Collections.Array<Node>(GetTree().GetNodesInGroup("AllyIndicators"));
-		for(int i = 0; i < enemyIndicators.Count; i++) {
-			Node indicatorParent = enemyIndicators[i].GetParent();
-			if(indicatorParent != null) {
-				indicatorParent.RemoveChild(enemyIndicators[i]);
-				SpareEnemyIndicators.Push(enemyIndicators[i]);
-			}
-		}
-		for(int i = 0; i < allyIndicators.Count; i++) {
-			Node indicatorParent = allyIndicators[i].GetParent();
-			if(indicatorParent != null) {
-				indicatorParent.RemoveChild(allyIndicators[i]);
-				SpareAllyIndicators.Push(allyIndicators[i]);
-			}
-		}
+	public void DestructibleSpawned(Destructible newUnit) {
+		
 	}
 
 	private void PopulateIndicators() {
+		Godot.Collections.Array<Destructible> units = new Godot.Collections.Array<Destructible>(GetTree().GetNodesInGroup("Destructible"));
+		for(int i = 0; i < units.Count; i++) {
 
-	}
-
-	private Vector2 PositionIndicator(Vector3 indicatorPosition3D) {
-		Vector2 position = new Vector2();
-		return position;
+		}
 	}
 }
