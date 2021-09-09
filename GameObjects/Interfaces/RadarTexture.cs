d23f15128041425fc7334a4deef49bd555269370
avinanick <avinanick@gmail.com>
2021-09-08 22:31:35 -0700

    Updated the radartexture class to keep the player view indicator up to date on the radar.

--- GameObjects/Interfaces/RadarTexture.cs
+++ GameObjects/Interfaces/RadarTexture.cs
@@ -10,6 +10,7 @@ public class RadarTexture : TextureRect
 	private PackedScene IndicatorScene = (PackedScene)GD.Load("res://GameObjects/Interfaces/RadarIndicator.tscn");
 
 	private Light2D PlayerView;
+	private Avatar PlayerAvatar;
 
 
 	// Called when the node enters the scene tree for the first time.
@@ -18,13 +19,19 @@ public class RadarTexture : TextureRect
 		base._Ready();
 		PlayerView = GetNode<Light2D>("PlayerView");
 		CallDeferred("PopulateIndicators");
+		CallDeferred("AssignPlayer");
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		UpdateAvatarIndicator();
+	}
+
+	public void AssignPlayer() {
+		PlayerAvatar = new Godot.Collections.Array<Avatar>(GetTree().GetNodesInGroup("Player"))[0];
+	}
 
 	public void DestructibleSpawned(Destructible newUnit) {
 		RadarIndicator newIndicator = (RadarIndicator)IndicatorScene.Instance();
@@ -37,5 +44,16 @@ public class RadarTexture : TextureRect
 		for(int i = 0; i < units.Count; i++) {
 			DestructibleSpawned(units[i]);
 		}
+	}
+
+	private void UpdateAvatarIndicator() {
+		if(PlayerAvatar != null) {
+            Vector3 unitPosition = PlayerAvatar.GlobalTransform.origin;
+            Vector2 convertedPosition = new Vector2();
+            convertedPosition.x = (-1) * unitPosition.x * 1 + 50;
+            convertedPosition.y = (-1) * unitPosition.z * 1 + 50;
+            PlayerView.Position = convertedPosition;
+			PlayerView.RotationDegrees = (-1) * PlayerAvatar.RotationDegrees.y;
+        }
 	}
 }
