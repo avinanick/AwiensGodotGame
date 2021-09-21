c2db72f7d0f89ef7da0d269c34fc6b282bacbf21
avinanick <avinanick@gmail.com>
2021-09-20 21:47:48 -0700

    Added initial member variables to the threat randomizer for tracking what threats are valid to be assigned.

--- GameSystems/ThreatRandomizer.cs
+++ GameSystems/ThreatRandomizer.cs
@@ -3,9 +3,20 @@ using System;
 
 public class ThreatRandomizer : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    // I want an array for enemy types, separated into difficulties, and one
+    // for enemy buffs, also separated into difficulties
+    // I'll likely make and use data singletons for this, that seems best
+    private string[] EasyAliens = new string[1] {"Alien Missile"};
+    private string[] MediumAliens = new string[0] {};
+    private string[] HardAliens = new string[0] {};
+    private string[] EasyModifiers = new string[0] {};
+    private string[] MediumModifiers = new string[0] {};
+    private string[] HardModifiers = new string[0] {};
+    private int MediumAlienThreshold = 3;
+    private int HardAlienThreshold = 6;
+    private int EasyModifierThreshold = 2;
+    private int MediumModifierThreshold = 5;
+    private int HardModifierThreshold = 8;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -24,6 +35,6 @@ public class ThreatRandomizer : Node
     }
 
     private void ReadThreats() {
-        
+
     }
 }
