66b226c34fac5b620848b632ab7ee6eb8396a91d
avinanick <avinanick@gmail.com>
2021-09-22 21:36:49 -0700

    Added a stub method for attaching the threats preview interface to a location.

--- GameObjects/Interfaces/ThreatsPreviewInterface.cs
+++ GameObjects/Interfaces/ThreatsPreviewInterface.cs
@@ -6,6 +6,7 @@ public class ThreatsPreviewInterface : MarginContainer
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private PackedScene IconPackedScene = GD.Load<PackedScene>("res://GameObjects/Interfaces/ThreatIcon.tscn");
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -23,11 +24,15 @@ public class ThreatsPreviewInterface : MarginContainer
         // This should get the icon for the threat and add it to the appropriate list
     }
 
+    public void AttachAtLocation(Vector2 location) {
+
+    }
+
     public void ClearThreats() {
         // Remove all threats, used when switching to another city
     }
 
     public void SetCityName(string cityName) {
-        
+
     }
 }
