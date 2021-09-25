10d47619a0cf406b95e579939746115ad1ad5bd0
avinanick <avinanick@gmail.com>
2021-09-25 11:47:43 -0700

    Added basic member variables to the research tree node class.

--- GameObjects/Interfaces/ResearchTreeNode.cs
+++ GameObjects/Interfaces/ResearchTreeNode.cs
@@ -6,6 +6,13 @@ public class ResearchTreeNode : Button
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private Button[] Neighbors;
+    private Line2D[] Lines;
+    private Line2D[] NeighborLines;
+    private float Delay = 10f;
+
+    [Export]
+    private int Cost = 0;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
