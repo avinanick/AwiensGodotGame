433b29f7b6af91ea14076bed8c0687c6a1cb63e4
avinanick <avinanick@gmail.com>
2021-09-25 14:16:50 -0700

    The research interface should make itself invisible on the game start.

--- GameObjects/Interfaces/ResearchInterface.cs
+++ GameObjects/Interfaces/ResearchInterface.cs
@@ -10,7 +10,7 @@ public class ResearchInterface : MarginContainer
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        Visible = false;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
