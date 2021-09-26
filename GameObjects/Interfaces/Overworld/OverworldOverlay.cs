e37a30075adced67f3b21e9a5e1d0a5a7d622daa
avinanick <avinanick@gmail.com>
2021-09-25 19:59:33 -0700

    Created a close menus utility function in the overworld overlay class to close all current menus.

--- GameObjects/Interfaces/Overworld/OverworldOverlay.cs
+++ GameObjects/Interfaces/Overworld/OverworldOverlay.cs
@@ -20,6 +20,13 @@ public class OverworldOverlay : MarginContainer
 //      
 //  }
 
+    private void CloseMenus() {
+        Godot.Collections.Array<Control> menus = new Godot.Collections.Array<Control>(GetNode<Node>("VBoxContainer/MarginContainer").GetChildren());
+        for(int i = 0; i < menus.Count; i++) {
+            menus[i].Visible = false;
+        }
+    }
+
     public void ItemsClicked() {
 
     }
