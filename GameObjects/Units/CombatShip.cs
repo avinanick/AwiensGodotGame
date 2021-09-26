8defa4a1251d184434dd7336d8856a185924225a
avinanick <avinanick@gmail.com>
2021-09-26 16:33:27 -0700

    Added an exported array to the combat ship that allows the scene to define what groups the ship will target.

--- GameObjects/Units/CombatShip.cs
+++ GameObjects/Units/CombatShip.cs
@@ -6,6 +6,8 @@ public class CombatShip : Ship
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    protected string[] TargetGroups;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -20,6 +22,6 @@ public class CombatShip : Ship
 //  }
 
     public override void SpawnShip() {
-        
+
     }
 }
