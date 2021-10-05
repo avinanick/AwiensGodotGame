e5cfcbdb81c1d17c41814a5946d26f9c01d06720
avinanick <avinanick@gmail.com>
2021-10-04 22:06:05 -0700

    Set up the portion of the strafe movement in the combat ship that checks to see if it needs to switch to approaching target.

--- GameObjects/Units/CombatShip.cs
+++ GameObjects/Units/CombatShip.cs
@@ -21,6 +21,8 @@ public class CombatShip : Ship
     protected AttackerComponent Weapon;
     protected Destructible CurrentTarget;
     protected Godot.Collections.Array<Destructible> PotentialTargets = new Godot.Collections.Array<Destructible>();
+    protected bool ApproachingTarget = true;
+    protected float TurnaroundDistance = 40;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -81,6 +83,17 @@ public class CombatShip : Ship
     protected void UpdateShipDirectionStrafe() {
         // This needs to be a movement pattern that moves toward the city, passes over it
         // continues on, then turns around and repeats.
+        // For now I'm  going to try: if approaching the city and not currently facing it, 
+        // turn to face the city. If approaching and facing, move toward the city until reaching
+        // it. If not approaching, move away until reaching a distance of 40
+        if(ApproachingTarget) {
+
+        }
+        else {
+            if(GlobalTransform.origin.DistanceTo(new Vector3(0,GlobalTransform.origin.y, 0)) >= TurnaroundDistance) {
+                ApproachingTarget = true;
+            }
+        }
     }
 
     protected void UpdateShipDirectionOrbit() {
