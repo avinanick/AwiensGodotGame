b311fdda5aa9d8abf3b68e6ef1ce2e5f6da6b247
avinanick <avinanick@gmail.com>
2021-10-12 16:42:49 -0700

    Added stub methods to the alien scout class, with some filled in based on the impeder.

--- GameObjects/Units/Aliens/AlienScout.cs
+++ GameObjects/Units/Aliens/AlienScout.cs
@@ -6,11 +6,13 @@ public class AlienScout : CombatShip
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    protected float HoverDistance = 9f;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +20,32 @@ public class AlienScout : CombatShip
 //  {
 //      
 //  }
+
+    public override void DestroySelf() {
+        base.DestroySelf();
+        EndTargeting();
+    }
+
+    protected void EndTargeting() {
+        
+    }
+
+    protected override void HoverDestinationReached() {
+        // Start up the impedence
+        base.HoverDestinationReached();
+        StartTargeting();
+    }
+
+    protected override void SetHoverLocation()
+    {
+        if(CurrentTarget != null & Godot.Object.IsInstanceValid(CurrentTarget)) {
+            base.SetHoverLocation();
+            Vector3 hoverOffsetDirection = (GlobalTransform.origin - CurrentTarget.GlobalTransform.origin).Normalized();
+            HoverLocation = CurrentTarget.GlobalTransform.origin + HoverDistance * hoverOffsetDirection;
+        }
+    }
+
+    protected void StartTargeting() {
+        
+    }
 }
