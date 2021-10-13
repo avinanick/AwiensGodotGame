f65084cf6f1e206cc584fc05d63e06cc9bf52100
avinanick <avinanick@gmail.com>
2021-10-13 15:14:52 -0700

    First pass at the fire ordinance method in the alien artillery class.

--- GameObjects/Units/Aliens/AlienArtillery.cs
+++ GameObjects/Units/Aliens/AlienArtillery.cs
@@ -6,8 +6,12 @@ public class AlienArtillery : CombatShip
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    Vector3 MissileSpawnOffset = new Vector3(0,3,0);
+
     protected PackedScene MissileScene = GD.Load<PackedScene>("res://GameObjects/Units/Aliens/AlienMissile.tscn");
     protected bool Bombarding = false;
+    protected AlienMissile CurrentMissile = null;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -22,16 +26,28 @@ public class AlienArtillery : CombatShip
 //  }
 
     protected void EndBombardment() {
-
+        Bombarding = false;
     }
 
     protected void FireOrdinance() {
-
+        if(Bombarding) {
+            // Calculate the missile spawn, then charge and launch it.
+            //Vector3 missileSpawnPoint = GlobalTransform.origin + MissileSpawnOffset;
+            AlienMissile newMissile = MissileScene.Instance<AlienMissile>();
+            AddChild(newMissile);
+            newMissile.Translation = MissileSpawnOffset;
+            CurrentMissile = newMissile;
+            newMissile.ChargeMissile();
+        }
     }
 
     protected override void HoverDestinationReached() {
         base.HoverDestinationReached();
         StartBombardment();
+    }
+
+    public void LaunchMissile() {
+        
     }
 
     protected override void SetHoverLocation()
