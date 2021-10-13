97d70ba5533be6a8c268fc5a6d8be8401b5a5b9a
avinanick <avinanick@gmail.com>
2021-10-13 14:44:54 -0700

    Added member variables and stub methods to the alien artillery class.

--- GameObjects/Units/Aliens/AlienArtillery.cs
+++ GameObjects/Units/Aliens/AlienArtillery.cs
@@ -6,6 +6,8 @@ public class AlienArtillery : CombatShip
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    protected PackedScene MissileScene = GD.Load<PackedScene>("res://GameObjects/Units/Aliens/AlienMissile.tscn");
+    protected bool Bombarding = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -18,4 +20,25 @@ public class AlienArtillery : CombatShip
 //  {
 //      
 //  }
+
+    protected void EndBombardment() {
+
+    }
+
+    protected void FireOrdinance() {
+
+    }
+
+    protected override void HoverDestinationReached() {
+        base.HoverDestinationReached();
+    }
+
+    protected override void SetHoverLocation()
+    {
+        base.SetHoverLocation();
+    }
+
+    protected void StartBombardment() {
+
+    }
 }
