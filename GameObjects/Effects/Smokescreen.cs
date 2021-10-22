db77a37806e206263c23ed304621cf104f865545
avinanick <avinanick@gmail.com>
2021-10-21 20:46:54 -0700

    Created a timer and script around the smokescreen particles to delete them after 10 seconds, not a nice solution, but it should work I guess.

---
+++ GameObjects/Effects/Smokescreen.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Smokescreen : Particles
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+    public void Blast() {
+        GetNode<Timer>("Timer").Start();
+    }
+
+    public void Finish() {
+        QueueFree();
+    }
+}
