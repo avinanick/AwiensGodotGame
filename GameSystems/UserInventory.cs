78a5010ca303de2a0d3494bc6c2d267f3ce4790c
avinanick <avinanick@gmail.com>
2021-10-27 13:11:17 -0700

    Added member variables and stub methods to the user inventory script.

--- GameSystems/UserInventory.cs
+++ GameSystems/UserInventory.cs
@@ -6,6 +6,9 @@ public class UserInventory : Node
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    protected System.Collections.Generic.Dictionary<String, int> EquippedItems = new System.Collections.Generic.Dictionary<string, int>();
+    
+    protected System.Collections.Generic.Dictionary<String, int> InventoryItems = new System.Collections.Generic.Dictionary<string, int>();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -19,9 +22,33 @@ public class UserInventory : Node
 //      
 //  }
 
+    public void AddItem(String itemName, int amount) {
+
+    }
+
+    public void EquipItem(String itemName, int amount = -1) {
+
+    }
+
+    public System.Collections.Generic.Dictionary<String, int> GetEquipped() {
+        return EquippedItems;
+    }
+
+    public System.Collections.Generic.Dictionary<String, int> GetInventory() {
+        return InventoryItems;
+    }
+
     public Godot.Collections.Dictionary<string, object> GetSaveDate() {
         return new Godot.Collections.Dictionary<string, object>() {
 
         };
+    }
+
+    public void UnequipItem(String itemName, int amount = -1) {
+
+    }
+
+    public int UseItem(String itemName) {
+        return 0; // STUB
     }
 }
