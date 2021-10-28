e870254367ce915eb776b8c6e953015ee0c091f7
avinanick <avinanick@gmail.com>
2021-10-27 17:44:41 -0700

    Added an item set class to the item data script, based on the alien set class from alien data.

--- GameSystems/ItemData.cs
+++ GameSystems/ItemData.cs
@@ -6,6 +6,30 @@ public class ItemData : Node
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private class ItemSet
+    {
+        private string ItemScenePath;
+        private string ItemIconPath;
+        private float ItemSpawnInterval;
+
+        public ItemSet(string scenePath, string itemPath, float spawnInterval) {
+            ItemScenePath = scenePath;
+            ItemIconPath = itemPath;
+            ItemSpawnInterval = spawnInterval;
+        }
+
+        public string GetItemPath() {
+            return ItemIconPath;
+        }
+
+        public string GetScenePath() {
+            return ItemScenePath;
+        }
+
+        public float GetSpawnInterval() {
+            return ItemSpawnInterval;
+        }
+    }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
