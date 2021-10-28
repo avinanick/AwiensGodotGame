d5ee01983b508d6a90f46bb4ec52bc1db0e10c8c
avinanick <avinanick@gmail.com>
2021-10-27 17:37:22 -0700

    Added a call to the base ready function in the user inventory, just in case.

--- GameSystems/UserInventory.cs
+++ GameSystems/UserInventory.cs
@@ -13,7 +13,7 @@ public class UserInventory : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
