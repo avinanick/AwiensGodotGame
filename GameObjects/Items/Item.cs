e6b03d9592e78c018a731a0975946aeffb8a02d3
avinanick <avinanick@gmail.com>
2021-10-28 22:21:08 -0700

    Added a member variable to the item class.

--- GameObjects/Items/Item.cs
+++ GameObjects/Items/Item.cs
@@ -6,11 +6,13 @@ public class Item : Spatial
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    private float DeployTime = 1.5f;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,5 +28,6 @@ public class Item : Spatial
     public virtual void Deploy() {
         // Items should be spawned in the center of the city and launched straight up
         // then activated
+        Translation = new Vector3(0,0,0);
     }
 }
