0bbfd3dd3a0ac3c1f8984ab402c988c0cbfcdb28
avinanick <avinanick@gmail.com>
2021-10-29 16:36:30 -0700

    Added functionality to the bomb activate method.

--- GameObjects/Items/Bomb.cs
+++ GameObjects/Items/Bomb.cs
@@ -10,7 +10,7 @@ public class Bomb : Item
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +18,17 @@ public class Bomb : Item
 //  {
 //      
 //  }
+
+    protected override void Activate() {
+        // This should get all destructibles in the alien group and deal heavy damage
+        // to them all
+        // Should I deal heavy damage or just call destroy self on each?
+        base.Activate();
+        Godot.Collections.Array<Destructible> alienTargets = new Godot.Collections.Array<Destructible>(GetTree().GetNodesInGroup("Aliens"));
+        for(int i = 0; i < alienTargets.Count; i++) {
+            // I may go with dealing damage instead, but for now lets go with instant kills.
+            alienTargets[i].DestroySelf();
+        }
+        QueueFree();
+    }
 }
