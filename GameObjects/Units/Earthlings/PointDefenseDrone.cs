e717dd23f5ea0f228aa13a02ab22c4450cdb681d
avinanick <avinanick@gmail.com>
2021-11-03 21:53:30 -0700

    Added function to the point defense drone method responding to a body entering the projectile kill area, and added a method for the cooldown timer.

--- GameObjects/Units/Earthlings/PointDefenseDrone.cs
+++ GameObjects/Units/Earthlings/PointDefenseDrone.cs
@@ -6,6 +6,10 @@ public class PointDefenseDrone : CombatShip
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    private float ProjectileKillCooldown = 1.0f;
+
+    private bool ReadyToBlock = true;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -19,7 +23,18 @@ public class PointDefenseDrone : CombatShip
 //      
 //  }
 
+    public void CooldownTimeout() {
+        ReadyToBlock = true;
+    }
+
     public void OnAreaEntered(Node body) {
-        
+        if(body is AlienBullet bullet) {
+            if(ReadyToBlock) {
+                bullet.QueueFree();
+                ReadyToBlock = false;
+                GetNode<Timer>("CooldownTimer").Start(ProjectileKillCooldown);
+                // Some animation to destroy the bullet
+            }
+        }
     }
 }
