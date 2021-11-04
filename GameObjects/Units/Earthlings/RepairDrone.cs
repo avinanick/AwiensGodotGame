ff92e6be5d62af0f671731b47929a1057934303a
avinanick <avinanick@gmail.com>
2021-11-03 21:07:22 -0700

    Set the repair drone to not process when it has no current target.

--- GameObjects/Units/Earthlings/RepairDrone.cs
+++ GameObjects/Units/Earthlings/RepairDrone.cs
@@ -12,7 +12,7 @@ public class RepairDrone : CombatShip
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -70,11 +70,13 @@ public class RepairDrone : CombatShip
                 CurrentTarget = PickTarget(PotentialTargets);
                 if(CurrentTarget != null) {
                     if(AttackPattern == AttackPatternType.Hover) {
+                        SetProcess(true);
                         SetHoverLocation();
                     }
                 }
                 else {
                     GetNode<Timer>("RetryUpdateTimer").Start();
+                    SetProcess(false);
                 }
                 
             }
