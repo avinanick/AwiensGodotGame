a2ee40fea926aa9d9b2499fd59e83474c9719d50
avinanick <avinanick@gmail.com>
2021-11-03 21:45:11 -0700

    Created a stub method in the point defense drone for reactive to a body entering the area.

--- GameObjects/Units/Earthlings/PointDefenseDrone.cs
+++ GameObjects/Units/Earthlings/PointDefenseDrone.cs
@@ -10,7 +10,7 @@ public class PointDefenseDrone : CombatShip
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +18,8 @@ public class PointDefenseDrone : CombatShip
 //  {
 //      
 //  }
+
+    public void OnAreaEntered(Node body) {
+        
+    }
 }
