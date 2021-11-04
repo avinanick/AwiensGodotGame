e4db69a227e02c4fa695fd4472207ad842515873
avinanick <avinanick@gmail.com>
2021-11-03 21:22:22 -0700

    Created the scene and script that will be the point defense drone ship.

--- GameObjects/Units/Earthlings/PointDefenseDrone.cs
+++ GameObjects/Units/Earthlings/PointDefenseDrone.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class PointDefenseDroneItem : Item
+public class PointDefenseDrone : CombatShip
 {
     // Declare member variables here. Examples:
     // private int a = 2;
