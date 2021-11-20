422302c0acc4e9678b061441a95c43a8f72c573b
avinanick <avinanick@gmail.com>
2021-11-20 15:17:56 -0800

    Initial implimentation of the deploy method for the holographic decoy item.

--- GameObjects/Items/HolographicDecoyItem.cs
+++ GameObjects/Items/HolographicDecoyItem.cs
@@ -6,6 +6,7 @@ public class HolographicDecoyItem : Item
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private PackedScene HologramScene = GD.Load<PackedScene>("res://GameObjects/Units/Earthlings/Hologram.tscn");
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -21,6 +22,19 @@ public class HolographicDecoyItem : Item
 
     public override void Deploy() {
         // Need to figure out a way to set a position for this
-        
+        Godot.Collections.Array<Spatial> spawnPoints = new Godot.Collections.Array<Spatial>(GetTree().GetNodesInGroup("HoloPoint"));
+        spawnPoints.Shuffle();
+        for(int i = 0; i < spawnPoints.Count; i++) {
+            if(spawnPoints[i].GetChildCount() < 1) {
+                // Spawn the hologram here and return
+                Destructible newHologram = HologramScene.Instance<Destructible>();
+                spawnPoints[i].AddChild(newHologram);
+                newHologram.GlobalTransform = spawnPoints[i].GlobalTransform;
+                return;
+            }
+        }
+        // If we get here, there are no remaining spawn points
+        GD.Print("No spawn points available");
+        // I should likely re-add the item that would have been spawned to the inventory here
     }
 }
