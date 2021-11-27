a7394ebc6c4870567f5c4aa722474a904d01f258
avinanick <avinanick@gmail.com>
2021-11-26 18:51:02 -0800

    Copied the item slot container logic to the inventory unequipped container.

--- GameObjects/Interfaces/Overworld/InventoryUnequippedContainer.cs
+++ GameObjects/Interfaces/Overworld/InventoryUnequippedContainer.cs
@@ -6,6 +6,9 @@ public class InventoryUnequippedContainer : HBoxContainer
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Signal]
+    public delegate void MouseOver();
+    private bool MouseWasInsideAlready = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,9 +16,19 @@ public class InventoryUnequippedContainer : HBoxContainer
         base._Ready();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+        if(GetGlobalRect().HasPoint(GetGlobalMousePosition())) {
+            if(!MouseWasInsideAlready) {
+                // nothing!
+                MouseWasInsideAlready = true;
+                EmitSignal("MouseOver");
+            }
+        }
+        else {
+            MouseWasInsideAlready = false;
+        }
+    }
 }
