7bac298221f0eda8ed98affcdd7f6af05d832daf
avinanick <avinanick@gmail.com>
2021-11-28 15:43:39 -0800

    Added stub methods and variables to the store icon script.

--- GameObjects/Interfaces/Overworld/StoreIcon.cs
+++ GameObjects/Interfaces/Overworld/StoreIcon.cs
@@ -6,11 +6,15 @@ public class StoreIcon : MarginContainer
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private string ItemName;
+
+    [Signal]
+    public delegate void IconUsed(string itemName);
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +22,12 @@ public class StoreIcon : MarginContainer
 //  {
 //      
 //  }
+
+    public void AssignItem(string itemName) {
+
+    }
+
+    public void IconClicked() {
+        
+    }
 }
