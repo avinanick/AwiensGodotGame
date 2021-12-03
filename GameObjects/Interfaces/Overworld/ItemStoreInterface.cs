aeba69f5171765c228bb0e5856ad16025162ed8e
avinanick <avinanick@gmail.com>
2021-12-02 19:18:43 -0800

    Added first pass at the populate inventory method in the item store interface.

--- GameObjects/Interfaces/Overworld/ItemStoreInterface.cs
+++ GameObjects/Interfaces/Overworld/ItemStoreInterface.cs
@@ -6,6 +6,7 @@ public class ItemStoreInterface : MarginContainer
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    PackedScene IconScene = GD.Load<PackedScene>("res://GameObjects/Interfaces/Overworld/StoreIcon.tscn");
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -30,7 +31,22 @@ public class ItemStoreInterface : MarginContainer
     }
 
     private void PopulateInventory() {
-
+        UserInventory inventory = GetNode<UserInventory>("/root/UserInventoryAL");
+        System.Collections.Generic.Dictionary<string, int> unequippedItems = inventory.GetInventory();
+        Tuple<string, int>[] equippedItems = inventory.GetEquipped();
+        Container inventoryContainer = GetNode<Container>("PanelContainer/VBoxContainer/HBoxContainer/PanelContainer/VBoxContainer/ScrollContainer/VBoxContainer");
+        foreach(System.Collections.Generic.KeyValuePair<string, int> entry in unequippedItems) {
+            StoreIcon newIcon = IconScene.Instance<StoreIcon>();
+            newIcon.AssignItem(entry.Key, entry.Value);
+            inventoryContainer.AddChild(newIcon);
+            newIcon.Connect("IconUsed", this, nameof(MakeSale));
+        }
+        for(int i = 0; i < equippedItems.Length; i++) {
+            StoreIcon newIcon = IconScene.Instance<StoreIcon>();
+            newIcon.AssignItem(equippedItems[i].Item1, equippedItems[i].Item2);
+            inventoryContainer.AddChild(newIcon);
+            newIcon.Connect("IconUsed", this, nameof(MakeSale));
+        }
     }
 
     private void RandomizeAvailable() {
